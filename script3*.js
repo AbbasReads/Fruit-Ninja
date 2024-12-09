23b46f8cdd4a5bd07e6f732db196648db32dd535
Abbas Mehdi <abbasmehdi3727@gmail.com>
2024-12-09 20:41:58 +0530

    Good to look at

--- script3*.js
+++ script3*.js
@@ -2,88 +2,92 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
-let background=new Image();
-background.src='fruit-ninja-assets/background.png';
-let names=['app','ban','or','wat','pin','bom'];
+let background = new Image();
+background.src = 'fruit-ninja-assets/background.png';
+let names = ['app', 'ban', 'or', 'wat', 'pin', 'bom'];
+let points = [1, 1, 2, 2, 5, 0];
 let arr = [];
 const gravity = 0.2;
-let score=0
-let missed=0
-let toStop=false;
-let active_splashes=[];
-let splash=[];
+let score = 0
+let missed = 0
+// let toStop=false;
+let toStop = 2;
+let active_splashes = [];
+let splash = [];
+let difficulty = 3000;
 const appleSP = new Image();
 appleSP.src = "fruit-ninja-assets/splash_red_small.png";
 splash.push(appleSP);
 const bananaSP = new Image();
-bananaSP.src="fruit-ninja-assets/splash_yellow.png";
+bananaSP.src = "fruit-ninja-assets/splash_yellow.png";
 splash.push(bananaSP);
 const orangeSP = new Image();
-orangeSP.src="fruit-ninja-assets/splash_orange.png";
+orangeSP.src = "fruit-ninja-assets/splash_orange.png";
 splash.push(orangeSP);
 const watermelonSP = new Image();
-watermelonSP.src="fruit-ninja-assets/splash_red.png";
+watermelonSP.src = "fruit-ninja-assets/splash_red.png";
 splash.push(watermelonSP);
 const pineappleSP = new Image();
-pineappleSP.src="fruit-ninja-assets/splash_yellow.png";
+pineappleSP.src = "fruit-ninja-assets/splash_yellow.png";
 splash.push(pineappleSP);
 const bombSP = new Image();
-bombSP.src='fruit-ninja-assets/explosion.png';
+bombSP.src = 'fruit-ninja-assets/explosion.png';
 splash.push(bombSP);
 
-let fruits=[]
+let fruits = []
 const apple = new Image();
 apple.src = "fruit-ninja-assets/apple.png";
 fruits.push(apple);
 const banana = new Image();
-banana.src="fruit-ninja-assets/banana.png";
+banana.src = "fruit-ninja-assets/banana.png";
 fruits.push(banana);
 const orange = new Image();
-orange.src="fruit-ninja-assets/orange.png";
+orange.src = "fruit-ninja-assets/orange.png";
 fruits.push(orange);
 const watermelon = new Image();
-watermelon.src="fruit-ninja-assets/watermelon.png";
+watermelon.src = "fruit-ninja-assets/watermelon.png";
 fruits.push(watermelon);
-const pineapple=new Image();
-pineapple.src='fruit-ninja-assets/pineapple.png';
+const pineapple = new Image();
+pineapple.src = 'fruit-ninja-assets/pineapple.png';
 fruits.push(pineapple)
 const bomb = new Image();
-bomb.src='fruit-ninja-assets/bomb.png';
+bomb.src = 'fruit-ninja-assets/bomb.png';
 fruits.push(bomb);
 
 // Fill background
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor({ position, velocity, type,NAME}) {
-        this.NAME=NAME;
+    constructor({ position, velocity, type, NAME, point }) {
+        this.NAME = NAME;
         this.position = position;
         this.velocity = velocity;
         this.type = type;
+        this.point = point;
         this.width = 65; // Sprite width
         this.height = 65; // Sprite height
-        if(names.indexOf(this.NAME)==4)
-            this.height=95;
+        if (names.indexOf(this.NAME) == 4)
+            this.height = 95;
         this.markedForDeletion = false;
     }
 
     update() {
-        c.drawImage(this.type,this.position.x, this.position.y,this.width+5,this.height+5)
-        if(this.velocity.x!=0&&this.velocity.y!=0){
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        this.velocity.y += gravity;
-        if (this.position.y > canvas.height) {
-            this.markedForDeletion = true;
-            if(this.type.src!='http://127.0.0.1:3000/fruit-ninja-assets/bomb.png')
-                missed++;
-            if(missed==3){
-             toStop=true;
+        c.drawImage(this.type, this.position.x, this.position.y, this.width + 5, this.height + 5)
+        if (this.velocity.x != 0 && this.velocity.y != 0) {
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+            this.velocity.y += gravity;
+            if (this.position.y > canvas.height) {
+                this.markedForDeletion = true;
+                if (this.NAME != 'bom')
+                    missed++;
+                if (missed == 3) {
+                    toStop = 0;
+                }
+                document.getElementById('missed').textContent = "Missed: " + missed;
+                // console.log("Missed:",missed);
             }
-            document.getElementById('missed').textContent="Missed: "+missed;
-            // console.log("Missed:",missed);
         }
-    }
     }
 }
 
@@ -96,12 +100,26 @@ function createFruits() {
         let velY = Math.random() * 7 + 8;
         let X2 = Math.random() * ((canvas.width - X) / (2 * velY) * gravity);
         let velX = X > canvas.width / 2 ? -X2 : X2;
-        let rand=Math.floor(Math.random() * fruits.length)
+        // let rand=Math.floor(Math.random() * fruits.length)
+        let rand = Math.random();
+        if (rand <= .25)
+            rand = 0;
+        else if (rand <= .5)
+            rand = 1;
+        else if (rand <= 0.75)
+            rand = 5;
+        else if (rand <= .8)
+            rand = 4;
+        else if (rand <= .9)
+            rand = 2;
+        else
+            rand = 3;
         let fruit = new Sprite({
             position: { x: X, y: canvas.height },
             velocity: { x: velX, y: -velY },
             type: fruits[rand],
-            NAME: names[rand]
+            NAME: names[rand],
+            point: points[rand]
         });
         // console.log(fruit.type);
         arr.push(fruit);
@@ -115,7 +133,7 @@ let isDragging = false;
 
 canvas.addEventListener('mousedown', () => {
     isDragging = true;
-    canvas.style.cursor='url("fruit-ninja-assets/knife.png"), auto'
+    canvas.style.cursor = 'url("fruit-ninja-assets/knife.png"), auto'
 });
 
 canvas.addEventListener('mousemove', (event) => {
@@ -129,7 +147,7 @@ canvas.addEventListener('mousemove', (event) => {
                 mouse.x >= fruit.position.x &&
                 mouse.x <= fruit.position.x + fruit.width &&
                 mouse.y >= fruit.position.y &&
-                mouse.y <= fruit.position.y + fruit.height && fruit.markedForDeletion==false
+                mouse.y <= fruit.position.y + fruit.height && fruit.markedForDeletion == false
             ) {
                 let SPLASH = new Sprite({
                     position: { x: fruit.position.x, y: fruit.position.y },
@@ -137,17 +155,23 @@ canvas.addEventListener('mousemove', (event) => {
                     type: splash[names.indexOf(fruit.NAME)],
                     // type: splash[fruits.indexOf(fruit)]
                 });
-                // console.log(fruit.type.src)
+                console.log(fruit.NAME);
                 active_splashes.push(SPLASH);
-                console.log(fruit.type.src);
-                if(fruit.type.src=='http://127.0.0.1:3000/fruit-ninja-assets/bomb.png')
-                    toStop=true;
-                else{
-                score++;
-                // console.log("Score:", score)
-                document.getElementById('score').textContent="Score: "+score
-                fruit.markedForDeletion = true; // Mark for deletion
+                // console.log(fruit.type.src);
+                if (fruit.NAME == 'bom')
+                    toStop = 1;
+                else {
+                    score=score+fruit.point;
+                    if (difficulty > 1000)
+                        difficulty -= 200;
+                    else if (difficulty > 600)
+                        difficulty -= 40;
+                    clearInterval(intervalid);
+                    intervalid = setInterval(createFruits, difficulty);
+                    console.log(difficulty)
+                    document.getElementById('score').textContent = "Score: " + score
                 }
+                fruit.markedForDeletion = true;
             }
         });
     }
@@ -155,31 +179,35 @@ canvas.addEventListener('mousemove', (event) => {
 
 canvas.addEventListener('mouseup', () => {
     isDragging = false;
-    canvas.style.cursor='default';
+    canvas.style.cursor = 'default';
 });
 
 // Animation loop
 function animate() {
     // c.fillStyle = 'black';
     // c.fillRect(0, 0, canvas.width, canvas.height);
-    c.drawImage(background,0,0,canvas.width,canvas.height)
+    c.drawImage(background, 0, 0, canvas.width, canvas.height)
     arr = arr.filter(fruit => !fruit.markedForDeletion); // Remove deleted fruits
-    
-    active_splashes.forEach(spl=>spl.update())
+
+    active_splashes.forEach(spl => spl.update())
     arr.forEach(fruit => fruit.update());
-    if(!toStop)
-    requestAnimationFrame(animate); // Continue the loop
+    if (toStop == 2)
+        requestAnimationFrame(animate); // Continue the loop
+    else if (toStop == 1) {
+        requestAnimationFrame(animate);
+        toStop--;
+    }
     else
-    clearInterval(intervalid);
+        clearInterval(intervalid);
 }
 
 // Start generating fruits and animation
 
-let intervalid = setInterval(createFruits, 3000);
-if(toStop) {
+let intervalid = setInterval(createFruits, difficulty);
+if (toStop == 0) {
     // let over=document.getElementsByClassName('over');
     // over.textContent="GAME OVER."
     // console.log(document.body)
-    clearInterval(intervalid); 
+    clearInterval(intervalid);
 }
 animate();
